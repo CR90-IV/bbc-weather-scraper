06a673a6fd40a40d5a4d69791be923af6a636eda
CR90-IV <43439686+CR90-IV@users.noreply.github.com>
2022-07-17 00:14:54 +0100

    update to save with location names

--- src/bbc-weather-scraper.py
+++ src/bbc-weather-scraper.py
@@ -3,27 +3,27 @@ from datetime import datetime
 from apiwrapper import RestConsumer
 bbc_weather = RestConsumer(base_url="https://weather-broker-cdn.api.bbci.co.uk/en/", append_slash=True)
 
-locations = [
-    "2643743", # Greater London
-    "2634341", # City of Westminster
-    "2650225", # Edinburgh
-    "2653822", # Cardiff
-    "2655984", # Belfast
-    "2988507", # Paris
-    "4140963", # Washington DC
-    "105343",  # Jeddah
-    "2147714", # Sydney
-    ]
+locations = {
+    "2643743" : "Greater London",
+    "2634341" : "City of Westminster",
+    "2650225" : "Edinburgh",
+    "2653822" : "Cardiff",
+    "2655984" : "Belfast",
+    "2988507" : "Paris",
+    "4140963" : "Washington DC",
+    "105343"  : "Jeddah",
+    "2147714" : "Sydney"
+    }
 
-for location in locations:
-    response = bbc_weather.forecast.aggregated[location]()
+for location_id, location_name in locations.items():
+    response = bbc_weather.forecast.aggregated[location_id]()
 
     detailed = pd.json_normalize(response["forecasts"], ["detailed", "reports"], meta=[["detailed", "issueDate"], ["detailed", "lastUpdated"]])
-    detailed['location'] = "2643743"
-
+    detailed['location_id'] = location_id
+    detailed['location_name'] = location_name
 
     # Save as CSV
     time_now_string = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-    detailed.to_csv("output/scrapes/detailed/scrape_{}_{}.csv".format(location, time_now_string), index=False)
+    detailed.to_csv("output/scrapes/detailed/scrape_{}_{}.csv".format(location_name.replace(" ",""), time_now_string), index=False)
 
 
