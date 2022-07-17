80929c3a9abc443af309481de4bd2727e3f0e049
CR90-IV <43439686+CR90-IV@users.noreply.github.com>
2022-07-17 11:15:41 +0100

    add scraping of summary tables

--- src/bbc-weather-scraper.py
+++ src/bbc-weather-scraper.py
@@ -15,15 +15,29 @@ locations = {
     "2147714" : "Sydney"
     }
 
+def append_location_details(df, location_id, location_name):
+    df['location_id'] = location_id
+    df['location_name'] = location_name
+    return df
+
+def save_scrape_to_csv(df, folder, label):
+    time_now_string = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
+    filepath = "{}/scrape_{}_{}.csv".format(folder, label.replace(" ",""), time_now_string)
+    df.to_csv(filepath, index=False)
+
 for location_id, location_name in locations.items():
     response = bbc_weather.forecast.aggregated[location_id]()
+    results = pd.DataFrame.from_records(response["forecasts"])
 
-    detailed = pd.json_normalize(response["forecasts"], ["detailed", "reports"], meta=[["detailed", "issueDate"], ["detailed", "lastUpdated"]])
-    detailed['location_id'] = location_id
-    detailed['location_name'] = location_name
+    # Detailed forecasts
+    detailed = pd.json_normalize(results["detailed"], ["reports"], ["issueDate", "lastUpdated"])
+    detailed = append_location_details(detailed, location_id, location_name)
+    save_scrape_to_csv(detailed, "output/scrapes/detailed", label=location_name)
 
-    # Save as CSV
-    time_now_string = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-    detailed.to_csv("output/scrapes/detailed/scrape_{}_{}.csv".format(location_name.replace(" ",""), time_now_string), index=False)
+    # Daily summaries
+    summary = pd.json_normalize(results["summary"])
+    summary = append_location_details(summary, location_id, location_name)
+    save_scrape_to_csv(summary, "output/scrapes/summary", label=location_name)
+
 
 
